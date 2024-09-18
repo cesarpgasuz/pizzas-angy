1ecca5af8f48a811decdf2e19bd06612a7f34817
Cesar Pablo Garcia Suarez <cesarpgasuz@gmail.com>
2024-09-18 16:04:26 -0600

    end: landingpage movil
    se termino la landingpage para la version movi, tambien se creo la funcion para mostrar el horario segun el dia de la semana

--- src/components/elements/Horario.jsx
+++ src/components/elements/Horario.jsx
@@ -1,9 +1,36 @@
+import { useMemo } from "react"
 import Clock from "../icons/Clock"
+import { HORARIOS } from "../../data"
+
+
+
 const Horario = () => {
+
+const fechaHoy = new Date().getDay()
+
+const calcularDia = (dia) => {
+
+  const semana = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado"
+  ];
+     return semana[dia]
+}
+
+
+const diaDeHoy = useMemo(() => calcularDia(fechaHoy), [fechaHoy])
+const horarioHoy = HORARIOS[diaDeHoy]
+
+
   return (
     <div className="w-[270px] px-3 py-2 bg-angy-orange-light rounded-md mx-auto flex justify-center items-center gap-2">
         <Clock className='size-6' />
-        <span className="font-semibold text-angy-texto-pri text-sm">Miercoles: 3:00 am - 10:00 pm</span>
+        <span className="font-semibold text-angy-texto-pri text-sm">{`${diaDeHoy}: ${horarioHoy}`}</span>
     </div>
   )
 }
--- src/components/sections/FormasPagos.jsx
+++ src/components/sections/FormasPagos.jsx
@@ -1,6 +1,22 @@
+import IconoBilletes from '../../assets/images/dolares.png'
+import IconoTarjeta from '../../assets/images/tarjeta-de-credito.png'
 const FormasPagos = () => {
   return (
-    <div>FormasPagos</div>
+    <section className="contenedor padding-25">
+            <h2 className="uppercase text-center text-angy-texto-pri font-bold text-xl mb-3">formas de pago</h2>
+            <p className="text-center font-medium text-angy-texto-sec text-xs mb-6">Contamos con  diferentes formas de pago</p>
+
+            <div className="grid grid-cols-2">
+              <div className='flex flex-col justify-center items-center'><img src={IconoBilletes} alt='imagen de unos billetes' className='size-12' />
+                <h3 className='text-xs font-bold text-angy-texto-sec'>Pago en Efectivo</h3>
+              </div>
+              <div className='flex flex-col justify-center items-center'>
+                <img src={IconoTarjeta} alt='imagen de una tarjeta de credio' className='size-12' />
+                <h3 className='text-xs font-bold text-angy-texto-sec'>Pago con Tarjeta</h3>
+              </div>
+            </div>
+   
+    </section>
   )
 }
 export default FormasPagos
